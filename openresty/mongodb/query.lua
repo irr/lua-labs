31d7017f41403a554ffb44d513bbde674a2e6254
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-01-22 13:11:31 -0200

    mongodb

---
+++ openresty/mongodb/query.lua
@@ -0,0 +1,24 @@
+-- https://github.com/bigplum/lua-resty-mongol
+
+local json = require 'cjson'
+ngx.header.content_type = 'application/json; charset=utf-8';
+
+local mongol = require "resty.mongol"
+local conn = mongol:new()
+
+conn:set_timeout(1000)
+
+ok, err = conn:connect("127.0.0.1", 27017)
+if ok then
+    local db = conn:new_db_handle("stock")
+    local col = db:get_col("symbols")
+    local r = col:find_one({S="UOLL4"})
+    local d = { ["package.path"] = package.path }
+    for _, v in pairs({"S", "D", "O", "H", "L", "C", "V"}) do
+        d[v] = r[v]
+    end
+    ngx.say(json.encode(d))
+    conn:close()
+else
+    ngx.say(json.encode({["package.path"]=package.path, error=err}))
+end
