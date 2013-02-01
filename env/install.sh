6df9adcecba6b5222c057023c2f7f1a407c7c946
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-02-01 15:00:18 -0200

    ubuntu

--- env/install.sh
+++ env/install.sh
@@ -2,12 +2,10 @@
 cd &&
 rm -rf .luarocks && 
 luarocks --local install concurrentlua && 
-luarocks --local install json4lua && 
 luarocks --local install lua-cjson && 
 luarocks --local install lua-llthreads && 
 luarocks --local install redis-lua && 
 luarocks --local install luasql-sqlite3 && 
 luarocks --local install luasql-mysql MYSQL_INCDIR=/usr/include/mysql MYSQL_LIBDIR=/usr/lib64/mysql &&
-luarocks --local install underscore.lua --from=http://marcusirven.s3.amazonaws.com/rocks &&
 luarocks --local list
 
