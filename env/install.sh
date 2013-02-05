6fb3e894dcd137c3b37a2056d0dbf335decae056
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-02-05 10:53:14 -0200

    mint

--- env/install.sh
+++ env/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd &&
 rm -rf .luarocks && 
-luarocks --local install concurrentlua && 
 luarocks --local install lua-cjson && 
-luarocks --local install lua-llthreads && 
 luarocks --local install redis-lua && 
 luarocks --local install luasql-sqlite3 && 
 luarocks --local install luasql-mysql MYSQL_INCDIR=/usr/include/mysql MYSQL_LIBDIR=/usr/lib64/mysql &&
