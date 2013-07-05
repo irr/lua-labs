7a664ffdcb21bbb60c29ecbd7b09e01ce1e9ce77
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-07-05 14:56:10 -0300

    openssl s_client

--- openresty/ssl-xca/ssl-router.lua
+++ openresty/ssl-xca/ssl-router.lua
@@ -4,6 +4,7 @@ nginx -s stop; nginx -c /home/irocha/lua/openresty/ssl-xca/nginx-https.conf
 curl -v -1 https://myirrlab.org:8443/ --cacert irrlab.crt
 curl -v -3 https://myirrlab.org:8443/ --cacert irrlab.crt
 
+openssl s_client -host myirrlab.org -port 8443 -CAfile irrlab.crt
 openssl s_client -host myirrlab.org -key myirrlab.org.pem -cert myirrlab.org.crt -port 8443
 --]]
 
