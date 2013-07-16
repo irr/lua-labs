e7ecbbe4022a1128116b784fa015d006c00d2652
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-07-16 14:48:18 -0300

    unsafe f

--- openresty/sync/index.lua
+++ openresty/sync/index.lua
@@ -1,17 +1,16 @@
 --[[
 nginx -s stop; nginx -c /home/irocha/lua/openresty/sync/nginx.conf
-nginx -s stop; nginx -c /home/irocha/lua/openresty/sync/nginx.conf && ab -n 100 -c 100 http://localhost:8888/ && curl -v http://localhost:8888/ && nginx -s stop && echo "DONE"
+nginx -s stop; nginx -c /home/irocha/lua/openresty/sync/nginx.conf && ab -n 1000 -c 100 http://localhost:8888/ && curl -v http://localhost:8888/ && nginx -s stop && echo "DONE"
 curl -v http://localhost:8888/
 --]]
 
 local sync = require("sync")
 
 local f = function (d)
-    local val, err = d:incr("COUNTER", 1)
-    if err then
-        d:set("COUNTER", 0)
-        val = 0
-    end
+    local val, _ = d:get("COUNTER")
+    val = (not val and 0) or (val + 1)
+    ngx.sleep(0.001)
+    d:set("COUNTER", val)
     return val
 end
 
