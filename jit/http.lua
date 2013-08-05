105c0ad67a254d0af73579e45b9fd2d6d799ea69
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-08-05 16:21:49 -0300

    centos

--- jit/http.lua
+++ jit/http.lua
@@ -16,7 +16,7 @@ const char* evhttp_request_uri(const struct evhttp_request *req);
 ]] 
 
 local ffi = require("ffi")
-local event = ffi.load("/usr/local/lib/libevent.so")
+local event = ffi.load("libevent-1.4.so.2")
 
 ffi.cdef[[
 typedef void (__stdcall *CB)(struct evhttp_request *, void *);
@@ -28,7 +28,7 @@ int event_base_dispatch(struct event_base *);
 void evhttp_send_reply(struct evhttp_request *req, int code, const char *reason, struct evbuffer *databuf);
 struct evbuffer *evbuffer_new(void);
 int evbuffer_add_printf(struct evbuffer *buf, const char *fmt, ...);
-const char* evhttp_request_get_uri(const struct evhttp_request *req);
+const char* evhttp_request_uri(const struct evhttp_request *req);
 ]]
 
 local base = event.event_init();
@@ -37,7 +37,7 @@ local port = 8081
 
 local cb = ffi.cast("CB", function(req, args)
     local buf = event.evbuffer_new();
-    event.evbuffer_add_printf(buf, "Requested: %s\n", event.evhttp_request_get_uri(req))
+    event.evbuffer_add_printf(buf, "Requested: %s\n", event.evhttp_request_uri(req))
     event.evhttp_send_reply(req, 200, "OK", buf)
 end)
 
