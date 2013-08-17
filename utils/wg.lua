a7a3902d75d8f9763b3de53a7d0dd1708eb8ff28
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-08-16 22:16:36 -0300

    os.execute

---
+++ utils/wg.lua
@@ -0,0 +1,59 @@
+#!/usr/bin/env lua
+
+function run(l, t, s, f)
+    local cmd, status = string.format("wget -c --timeout=%d ", t), nil
+
+    if #l > 0 then
+        cmd = cmd .. string.format("--limit-rate=%s ", l)
+    end
+
+    if f:find("http://") then
+        cmd = cmd .. f
+    elseif f:find("https://") then
+        cmd = cmd .. string.format("--no-check-certificate %s", f)
+    else
+        cmd = cmd .. string.format("--no-check-certificate --user=irr --password=0cean https://ocean:8080/%s", f)
+    end
+
+    repeat
+        _, status = pcall(os.execute, cmd)
+        if status ~= 0 then
+            print(string.format("wlua: waiting for %d seconds [%s]...", s, f))
+            if status ~= 2 then
+                pcall(os.execute, "sleep " .. tonumber(s))
+            end
+        end
+    until (status == 0) or (status == 2)
+
+    if status == 0 then
+        print(string.format("wlua: [%s] finished ok.", f))
+    else
+        print(string.format("wlua: [%s] was interrupted.", f))
+    end
+end
+
+local l, t, s, f = "", 5, 5, nil
+
+for i = 1, #arg do
+    if arg[i]:find("-t") == 1 and #arg[i] > 2 then
+        t = tonumber(arg[i]:sub(3))
+    elseif arg[i]:find("-s") == 1 and #arg[i] > 2 then
+        s = tonumber(arg[i]:sub(3))
+    elseif arg[i]:find("-l") == 1 and #arg[i] > 2 then
+        l = arg[i]:sub(3)
+    else
+        f = arg[i]
+    end
+end
+
+if f == nil then
+    print("Usage: wlua.lua [OPTIONS] <file or url (http/https)>")
+    print("       -l<s> = limit rate")
+    print("       -t<n> = timeout in seconds")
+    print("       -s<n> = retry interval in seconds")
+    os.exit(1)
+end
+
+print(string.format("wlua: downloading [%s] with timeout=[%d] and retry=[%d]...", f, t, s))
+
+run(l, t, s, f)
