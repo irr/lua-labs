053dde2a9c0c5e24f521fb210f8348807d22a921
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-11-11 16:30:57 -0200

    luarocks

--- configs/init.lua
+++ configs/init.lua
@@ -1,2 +1,4 @@
-require("luarocks.loader")
+package.path = package.path .. ";/opt/lua/luarocks/share/lua/5.1/?.lua;"
+package.cpath = package.cpath .. ";/opt/lua/luarocks/lib/lua/5.1/?.so;"
+--require("luarocks.loader")
 _ = require("underscore")
