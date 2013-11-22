2db11df65d036cbf05cb8f1109381f947be62fb3
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-11-22 05:53:09 -0200

    utils

--- configs/init.lua
+++ configs/init.lua
@@ -1,10 +1,31 @@
 package.path = package.path .. ";/opt/lua/luarocks/share/lua/5.1/?.lua;"
 package.cpath = package.cpath .. ";/opt/lua/luarocks/lib/lua/5.1/?.so;"
---require("luarocks.loader")
+
 _ = require("underscore")
+
 function d(t)
     for k, v in pairs(t) do
         print(k,v)
     end
+end
+
+function os.capture(cmd, raw)
+    local f = assert(io.popen(cmd, 'r'))
+    local s = assert(f:read('*a'))
+    f:close()
+    if raw then return s end
+    s = string.gsub(s, '^%s+', '')
+    s = string.gsub(s, '%s+$', '')
+    s = string.gsub(s, '[\n\r]+', ' ')
+    return s
+end
+
+function split(str, sep)
+    local s = str..sep
+    return s:match((s:gsub('[^'..sep..']*'..sep, '([^'..sep..']*)'..sep)))
+end
+
+function trim(s)
+    return s:match '^%s*(.-)%s*$'
 end
 
