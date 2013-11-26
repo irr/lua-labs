d3dbd2194ef9eeb9808e807a129b006f745d228f
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-11-26 13:32:53 -0200

    fmt

--- luv/tcp.lua
+++ luv/tcp.lua
@@ -6,13 +6,11 @@ local p = require('utils').prettyPrint
 local uv = require('luv')
 
 local function create_server(host, port, on_connection)
-  local server = uv.new_tcp()
-  
-  p("create_server", server)
-  
-  uv.tcp_bind(server, host, port)
-  
-  function server:onconnection()
+    local server = uv.new_tcp()
+    p("create_server", server)
+    uv.tcp_bind(server, host, port)
+    
+    function server:onconnection()
         local client = uv.new_tcp()
         uv.accept(server, client)
         p("server:onconnection", client)
@@ -23,9 +21,8 @@ local function create_server(host, port, on_connection)
         p("server:onclose", "server closed")
     end
 
-  uv.listen(server)
-
-  return server
+    uv.listen(server)
+    return server
 end
 
 local server = create_server("0.0.0.0", 12345, function (client)
@@ -33,19 +30,22 @@ local server = create_server("0.0.0.0", 12345, function (client)
         function client:ondata(chunk)
             p("client:ondata", chunk)
             uv.write(client, chunk, function ()
-                p("uv.write:client", chunk)
+                    p("uv.write:client", chunk)
             end)
         end
+        
         function client:onend()
             p("client:onend")
             uv.shutdown(client, function ()
-            p("uv.shutdown")
-            uv.close(client)
+                p("uv.shutdown")
+                uv.close(client)
             end)
         end
+        
         function client:onclose()
             p("client:onclose")
         end
+            
         uv.read_start(client)
     end)
 
@@ -54,7 +54,7 @@ local address = uv.tcp_getsockname(server)
 p("server", server, address)
 
 repeat
-  print("\ntick..")
+    print("\ntick..")
 until uv.run('once') == 0
 
 print("done")
