3205cf7f58d26d1b4e50962eac645c300d05c037
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-12-10 14:02:36 -0200

    centos-6.5

--- env/install.sh
+++ env/install.sh
@@ -2,8 +2,9 @@
 cd &&
 rm -rf .luarocks && 
 luarocks --local install lua-cjson && 
+luarocks --local install lua-iconv && 
 luarocks --local install redis-lua && 
-luarocks --local install luasql-sqlite3 && 
+luarocks --local install lualogging && 
 luarocks --local install luasql-mysql MYSQL_INCDIR=/usr/include/mysql MYSQL_LIBDIR=/usr/lib64/mysql &&
+luarocks --local install underscore.lua --from=http://marcusirven.s3.amazonaws.com/rocks/ && 
 luarocks --local list
-
