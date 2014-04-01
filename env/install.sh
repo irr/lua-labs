d91a4c1ab7309450b99aba515ccafd1ab3f535ef
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-03-31 21:31:20 -0300

    docs

--- env/install.sh
+++ env/install.sh
@@ -4,7 +4,5 @@ rm -rf .luarocks &&
 luarocks --local install lua-cjson && 
 luarocks --local install lua-iconv && 
 luarocks --local install redis-lua && 
-luarocks --local install lualogging && 
 luarocks --local install luasql-mysql MYSQL_INCDIR=/usr/include/mysql MYSQL_LIBDIR=/usr/lib64/mysql &&
-luarocks --local install underscore.lua --from=http://marcusirven.s3.amazonaws.com/rocks/ && 
 luarocks --local list
--- env/makedocs.sh
+++ env/makedocs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pandoc -S -o /opt/lua/docs/lua-nginx-module.html /opt/lua/lua-nginx-module/README.markdown 
+pandoc -S -o /opt/lua/docs/lua-resty-dns.html /opt/lua/lua-resty-dns/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-memcached.html /opt/lua/lua-resty-memcached/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-mysql.html /opt/lua/lua-resty-mysql/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-redis.html /opt/lua/lua-resty-redis/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-string.html /opt/lua/lua-resty-string/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-upload.html /opt/lua/lua-resty-upload/README.markdown 
-pandoc -S -o /opt/lua/docs/lua-resty-dns.html /opt/lua/lua-resty-dns/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-upstream.html /opt/lua/lua-resty-upstream/README.md
