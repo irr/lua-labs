e02effbc8ffe951c1b2e706a60da8be35bdbe312
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-03-31 23:07:19 -0300

    shell

--- env/makedocs.sh
+++ env/makedocs.sh
@@ -4,6 +4,7 @@ pandoc -S -o /opt/lua/docs/lua-resty-dns.html /opt/lua/lua-resty-dns/README.mark
 pandoc -S -o /opt/lua/docs/lua-resty-memcached.html /opt/lua/lua-resty-memcached/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-mysql.html /opt/lua/lua-resty-mysql/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-redis.html /opt/lua/lua-resty-redis/README.markdown 
+pandoc -S -o /opt/lua/docs/lua-resty-shell.html /opt/lua/lua-resty-shell/README.md 
 pandoc -S -o /opt/lua/docs/lua-resty-string.html /opt/lua/lua-resty-string/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-upload.html /opt/lua/lua-resty-upload/README.markdown 
 pandoc -S -o /opt/lua/docs/lua-resty-upstream.html /opt/lua/lua-resty-upstream/README.md
