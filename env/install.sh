c3c5a9bcc370c13ca6c29fc38ab0d4d90b1d38fd
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-04-25 10:46:38 -0300

    modules

--- env/install.sh
+++ env/install.sh
@@ -5,4 +5,7 @@ luarocks --local install lua-cjson &&
 luarocks --local install lua-iconv && 
 luarocks --local install redis-lua && 
 luarocks --local install luasql-mysql MYSQL_INCDIR=/usr/include/mysql MYSQL_LIBDIR=/usr/lib64/mysql &&
+luarocks --local install lualogging &&
+luarocks --local install luasec OPENSSL_LIBDIR=/usr/lib64/ &&
+luarocks --local install luasql-sqlite3
 luarocks --local list
