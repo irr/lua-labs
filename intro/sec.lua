6c865b35535c4d2291f1b169b7a416d05b4b6aaf
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-05-15 15:02:38 -0300

    chunk

--- intro/sec.lua
+++ intro/sec.lua
@@ -1,12 +1,16 @@
 local http = require "socket.http"
 local https = require "ssl.https"
+local r = {}
 http.TIMEOUT = 1
 b, c, h = https.request({
         url = "https://www.google.fr/",
-        sink = ltn12.sink.table(resp),
+        sink = ltn12.sink.table(r),
         protocol = "tlsv1",
         verify = "none"})
 print(b, c, h)
 for k, v in pairs(h) do
     print(k, v)
+end
+for k, _ in pairs(r) do
+    print("chunk: ", k)
 end
