fb307e144ae7a8f6659e4464c3243926a5707542
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-05-28 10:08:32 -0300

    dump

--- openresty/capture/index.lua
+++ openresty/capture/index.lua
@@ -1,10 +1,20 @@
-ngx.header.content_type = 'application/json'
-
 -- curl -v --user admin:admin123 http://localhost:8888/api/esm?limit=1
 -- curl -v --header "Authorization: Basic YWRtaW46YWRtaW4xMjM=" http://localhost:8888/api/esm?limit=1
 -- curl -v http://localhost:8888/api/esm?limit=1
 
+-- curl -v --header "Authorization: Basic YWRtaW46YWRtaW4xMjM=" http://esm/api/esm
+
 ngx.req.set_header("Authorization", ngx.var.basic)
 
 res = ngx.location.capture("/proxy" .. ngx.var.uri, { share_all_vars = true })
-ngx.print(res.body)
+
+for k, v in pairs(res.header) do
+    if type(v) == "table" then
+        for tk, tv in pairs(v) do
+            ngx.say("\t" .. tk .. ": " .. tostring(tv))
+        end        
+    end
+    ngx.say(k .. ": " .. tostring(v))
+end
+
+ngx.say("Body: " .. res.body:sub(1, 100) .. "...")
