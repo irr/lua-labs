313d92b326dab92d69de33d61d28eb2bca7ef0ef
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-06-26 11:56:24 -0300

    testmod

---
+++ intro/testmod.lua
@@ -0,0 +1,28 @@
+--[[
+test = require "testmod"
+test.info(1,2,"a",{a=100})
+]]
+
+local _M = {
+    _VERSION = '1.00'
+}
+
+local print = print
+local type = type
+local tostring = tostring
+local pairs = pairs
+
+setfenv(1, _M)
+
+local function debug(args)
+    for k, v in pairs(args) do
+        print(tostring(k)..":["..tostring(v).."] type of "..type(v))
+    end
+end
+
+function _M.info(...)  
+    print("testmod v" .._M._VERSION)
+    debug({...})
+end
+
+return _M
