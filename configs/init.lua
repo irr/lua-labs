82ee4941df76fe661cb1760a66326a13f7ade52e
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-10-07 13:52:15 -0300

    encode+decode+uri

--- configs/init.lua
+++ configs/init.lua
@@ -36,3 +36,37 @@ function expand(s)
                 return tostring(_G[n])
             end))
 end
+
+function unescape(s)
+    s = string.gsub(s, "+", " ")
+    s = string.gsub(s, "%%(%x%x)", function (h)
+            return string.char(tonumber(h, 16))
+        end)
+    return s
+end
+
+function escape(s)
+    s = string.gsub(s, "[&=+%%%c]", function (c)
+            return string.format("%%%02X", string.byte(c))
+        end)
+    s = string.gsub(s, " ", "+")
+    return s
+end
+
+function decode(s)
+    local cgi = {}
+    for name, value in string.gmatch(s, "([^&=]+)=([^&=]+)") do
+        name = unescape(name)
+        value = unescape(value)
+        cgi[name] = value
+    end
+    return cgi
+end
+
+function encode (t)
+    local b = {}
+    for k,v in pairs(t) do
+        b[#b + 1] = (escape(k) .. "=" .. escape(v))
+    end
+    return table.concat(b, "&")
+end
