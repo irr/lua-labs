96a9b068602ce614c88731174c19565e0635ca93
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-12-02 18:35:36 -0200

    meta

--- intro/meta.lua
+++ intro/meta.lua
@@ -2,6 +2,8 @@ Rectangle = {x = 100, y = 1000, width = 10, height = 20}
 Rectangle.__index = Rectangle
 
 function Rectangle:new (o)
+    local o = o or {}
+    print(o, self)
     setmetatable(o, self)
     return o
 end
