a56546d32e6ddad199a9df1481e3afb69e9925d6
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-12-11 11:13:54 -0200

    centos6

--- R/rserve.sh
+++ R/rserve.sh
@@ -2,4 +2,4 @@
 # install.packages('Rserve',,'http://www.rforge.net/')
 # library(Rserve)
 # Rserve(args="--no-save")
-R CMD /home/irocha/R/x86_64-redhat-linux-gnu-library/3.0/Rserve/libs//Rserve --no-save
+R CMD /home/irocha/R/x86_64-redhat-linux-gnu-library/3.1/Rserve/libs//Rserve --no-save
