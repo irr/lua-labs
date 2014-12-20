d6aff3c3f5a3f60d2af0e50541a9699683874a44
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-12-20 20:02:35 -0200

    irocha

--- env/post-install.sh
+++ env/post-install.sh
@@ -3,7 +3,7 @@ cd /usr/sbin
 sudo ln -s /opt/lua/openresty/nginx/sbin/nginx
 cd /usr/local/bin
 sudo ln -s /opt/lua/openresty/luajit/bin/luajit-2.1.0-alpha luajit
-sudo ln -s /opt/lua/openresty/luangx/luangx.lua luangx
+sudo ln -s /home/irocha/lua/openresty/luangx/luangx.lua luangx
 
 cd /usr/local
 sudo ln -s /opt/lua/openresty openresty
