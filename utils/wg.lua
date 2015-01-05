5016100270bb223b2be8af3c33ef2a8fcf4a8fe5
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-01-05 10:58:37 -0200

    ender

--- utils/wg.lua
+++ utils/wg.lua
@@ -1,10 +1,21 @@
-#!/usr/bin/env luajit
+#!/usr/bin/env lua
 
-function run(l, t, s, f)
+USER=os.getenv("U")
+PASSWORD=os.getenv("P")
+
+function run(l, n, t, s, f, o)
     local cmd, status = string.format("wget -c --timeout=%d ", t), nil
 
     if #l > 0 then
         cmd = cmd .. string.format("--limit-rate=%s ", l)
+    end
+
+    if #n > 0 then
+        cmd = cmd .. string.format("-O \"%s\" ", n)
+    end
+
+    if o ~= nil then
+        cmd = cmd .. string.format("-O \"%s\" ", o)
     end
 
     if f:find("http://") then
@@ -12,8 +23,7 @@ function run(l, t, s, f)
     elseif f:find("https://") then
         cmd = cmd .. string.format("--no-check-certificate %s", f)
     else
-        cmd = cmd .. string.format("--no-check-certificate --user=%s --password=%s \"https://enderseed.shell.seedhost.eu/enderseed/downloads/irr/%s\"", 
-              os.getenv("U"), os.getenv("P"), f)
+        cmd = cmd .. string.format("--no-check-certificate --user=%s --password=%s 'https://enderseed.buzz.seedhost.eu/enderseed/downloads/%s'", USER, PASSWORD, f)
     end
 
     repeat
@@ -33,7 +43,7 @@ function run(l, t, s, f)
     end
 end
 
-local l, t, s, f = "", 5, 5, nil
+local l, n, t, s, f, o, p, m = "", "", 5, 5, nil, nil, nil, nil
 
 for i = 1, #arg do
     if arg[i]:find("-t") == 1 and #arg[i] > 2 then
@@ -42,19 +52,38 @@ for i = 1, #arg do
         s = tonumber(arg[i]:sub(3))
     elseif arg[i]:find("-l") == 1 and #arg[i] > 2 then
         l = arg[i]:sub(3)
+    elseif arg[i]:find("-n") == 1 and #arg[i] > 2 then
+        n = arg[i]:sub(3)
+    elseif arg[i]:find("-m") == 1 and #arg[i] > 2 then
+        m = tonumber(arg[i]:sub(3))
     else
-        f = arg[i]
+        if f == nil then
+            f = arg[i]
+        else
+            o = arg[i]
+        end
     end
 end
 
 if f == nil then
-    print("Usage: wg [OPTIONS] <file or url (http/https)>")
+    print("Usage: wg [OPTIONS] <file or url (http/https)> [output-filename]")
     print("       -l<s> = limit rate")
     print("       -t<n> = timeout in seconds")
     print("       -s<n> = retry interval in seconds")
+    print("       -n<s> = output-filename")
+    print("       -m<n> = multiples-rars")
     os.exit(1)
 end
 
 print(string.format("wg: downloading [%s] with timeout=[%d] and retry=[%d]...", f, t, s))
 
-run(l, t, s, f)
+if m and m > 0 then
+    run(l, n, t, s, string.format("%s.rar", f))
+    for i = 0, m do
+        print(string.format("wg: downloading %02d/%02d...", i, m))
+        run(l, n, t, s, string.format("%s.r%02d", f, i))
+    end
+else
+    run(l, n, t, s, f, o)
+end
+
