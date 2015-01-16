92d6f86014dece9f2913b665c6a60d0c2578f082
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-01-15 22:57:43 -0200

    all]

--- env/install.sh
+++ env/install.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+sudo yum install -y readline-devel pcre-devel openssl-devel \
+                 sqlite-devel mysql-devel \
+                 zeromq3 zeromq3-devel
+sudo yum install -y perl-CPAN perl-Text-Diff perl-Test-LongString \
+                 perl-List-MoreUtils perl-Test-Base \
+                 perl-IO-Socket-SSLi perl-Time-HiRes
+cd /opt/lua
+git clone git@github.com:irr/nginx_tcp_proxy_module.git
+cd nginx_tcp_proxy_module
+git remote add upstream https://github.com/yaoweibin/nginx_tcp_proxy_module.git
+git fetch upstream && git merge upstream/master && git push
+cd ..
+wget http://openresty.org/download/ngx_openresty-1.7.7.1.tar.gz
+tar xfva ngx_openresty-1.7.7.1.tar.gz
+cd ngx_openresty-1.7.7.1
+patch -p1 < ../nginx_tcp_proxy_module/tcp-ngx-1.7.7.1.patch
+./configure --prefix=/opt/lua/openresty \
+            --with-luajit \
+            --with-http_realip_module \
+            --with-http_iconv_module \
+            --with-http_stub_status_module \
+            --with-debug --add-module=../nginx_tcp_proxy_module
+make install
+cd /usr/sbin
+sudo ln -s /opt/lua/openresty/nginx/sbin/nginx
+cd /usr/local/bin
+sudo ln -s /opt/lua/openresty/luajit/bin/luajit-2.1.0-alpha luajit
+sudo ln -s /home/irocha/lua/openresty/luangx/luangx.lua luangx
+cd /usr/local
+sudo ln -s /opt/lua/openresty openresty
+sudo ln -s /opt/lua/openresty openresty-debug
+ls -alF /usr/local/bin;echo
+sudo cp ~/lua/configs/luajit.conf /etc/ld.so.conf.d/
+sudo ldconfig && ldconfig -p | grep luaj
 cd /opt/lua
 if [ ! -d "luarocks" ]; then ln -s ~/.luarocks luarocks; fi
 git clone https://github.com/openresty/openresty.org.git
@@ -95,3 +129,18 @@ ln -s /opt/lua/luajit-examples
 ln -s /opt/lua/docker-openresty
 ln -s /opt/lua/openresty-docker
 cd
+luarocks --local install lua-cjson
+luarocks --local install lua-iconv
+luarocks --local install lua-llthreads2
+luarocks --local install luacrypto
+luarocks --local install lualogging
+luarocks --local install luasec OPENSSL_LIBDIR=/usr/lib64/
+luarocks --local install luasql-mysql \
+                         MYSQL_INCDIR=/usr/include/mysql \
+                         MYSQL_LIBDIR=/usr/lib64/mysql
+luarocks --local install luasql-sqlite3
+luarocks --local install lzmq
+luarocks --local install redis-lua
+luarocks --local install stdlib
+luarocks --local install underscore.lua \
+                         --from=http://github.com/irr/underscore.lua/raw/master/rocks
