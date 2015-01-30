dd28340d0c06ea9f618cb40b24fcdb2d805732d4
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-01-30 10:12:57 -0200

    antergos

--- env/install-arch.sh
+++ env/install-arch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "installing dependencies..."
-sudo pacman -S readline openssl pcre sqlite libmariadbclient
-sudo pacman -S cpanminus perl-text-diff perl-list-moreutils perl-lwp-protocol-https perl-test-base
+sudo pacman -S pcre libmariadbclient cpanminus perl-text-diff perl-list-moreutils perl-lwp-protocol-https perl-test-base
 echo "installing openresty..."
 cd /opt/lua
 git clone git@github.com:irr/nginx_tcp_proxy_module.git
@@ -34,8 +33,6 @@ echo "updating libraries..."
 sudo cp ~/lua/configs/luajit.conf /etc/ld.so.conf.d/
 sudo ldconfig && ldconfig -p | grep luaj
 cd /opt/lua
-if [ ! -d "luarocks-5.1" ]; then ln -s ~/.luarocks-5.1 luarocks-5.1; fi
-git clone https://github.com/openresty/openresty.org.git
 wget http://search.cpan.org/CPAN/authors/id/R/RG/RGARCIA/Test-LongString-0.17.tar.gz
 tar xfva Test-LongString-0.17.tar.gz
 cd Test-LongString-0.17
@@ -43,115 +40,19 @@ perl Makefile.PL
 make
 sudo make install
 cd ..
+rm -rf Test-LongString-0.17 Test-LongString-0.17.tar.gz
 git clone https://github.com/openresty/test-nginx.git
 cd test-nginx
 perl Makefile.PL
 make
 sudo make install
 cd ..
-git clone https://github.com/openresty/nginx-tutorials.git
-mkdir -p /opt/lua/docs
-mkdir -p /opt/lua/modules/nginx
-cd /opt/lua/modules/nginx/
-git clone https://github.com/openresty/headers-more-nginx-module.git
-git clone https://github.com/openresty/set-misc-nginx-module.git
-git clone https://github.com/calio/iconv-nginx-module.git
-git clone https://github.com/openresty/lua-nginx-module.git
-git clone https://github.com/openresty/lua-resty-dns.git
-git clone https://github.com/pintsized/lua-resty-http.git
-git clone https://github.com/openresty/lua-resty-memcached.git
-git clone https://github.com/openresty/lua-resty-mysql.git
-git clone https://github.com/openresty/lua-resty-redis.git
-git clone https://github.com/openresty/lua-resty-string.git
-git clone https://github.com/openresty/lua-resty-upload.git
-git clone https://github.com/openresty/lua-resty-upstream-healthcheck.git
-git clone https://github.com/hamishforbes/lua-resty-upstream.git
-git clone https://github.com/openresty/lua-resty-lrucache.git
-git clone https://github.com/bungle/lua-resty-template.git
-mkdir -p /opt/lua/modules/forked
-cd /opt/lua/modules/forked/
-git clone git@github.com:irr/lua-resty-shell.git
-cd lua-resty-shell
-git remote add upstream https://github.com/juce/lua-resty-shell.git
-git fetch upstream && git merge upstream/master && git push
-cd ..
-git clone git@github.com:irr/lua-pycrypto-aes.git
-cd lua-pycrypto-aes
-git remote add upstream https://github.com/siddontang/lua-pycrypto-aes.git
-git fetch upstream && git merge upstream/master && git push
-cd ..
+rm -rf test-nginx
 git clone git@github.com:irr/sockproc.git
 cd sockproc
 git remote add upstream https://github.com/juce/sockproc.git
 git fetch upstream && git merge upstream/master && git push
 make
 sudo mv sockproc /usr/local/bin/
-make clean
-cd /opt/lua
-git clone http://luajit.org/git/luajit-2.0.git
-cd luajit-2.0
-git checkout v2.1
 cd ..
-git clone git@github.com:irr/underscore.lua.git
-cd underscore.lua
-git remote add upstream https://github.com/mirven/underscore.lua.git
-git fetch upstream && git merge upstream/master && git push
-cd ..
-git clone git@github.com:irr/luajit-examples.git
-cd luajit-examples
-git remote add upstream https://github.com/hnakamur/luajit-examples.git
-git fetch upstream && git merge upstream/master && git push
-cd ..
-git clone git@github.com:irr/docker-openresty.git
-cd docker-openresty
-git remote add upstream https://github.com/3scale/docker-openresty.git
-git fetch upstream && git merge upstream/master && git push
-cd ..
-git clone git@github.com:irr/openresty-docker.git
-cd openresty-docker
-git remote add upstream https://github.com/torhve/openresty-docker.git
-git fetch upstream && git merge upstream/master && git pushlibmariadbclient
-cd ~/git
-ln -s /opt/lua/modules/nginx/headers-more-nginx-module
-ln -s /opt/lua/modules/nginx/set-misc-nginx-module
-ln -s /opt/lua/modules/nginx/iconv-nginx-module
-ln -s /opt/lua/modules/nginx/lua-nginx-module
-ln -s /opt/lua/modules/nginx/lua-resty-dns
-ln -s /opt/lua/modules/nginx/lua-resty-http
-ln -s /opt/lua/modules/nginx/lua-resty-memcached
-ln -s /opt/lua/modules/nginx/lua-resty-mysql
-ln -s /opt/lua/modules/nginx/lua-resty-redis
-ln -s /opt/lua/modules/nginx/lua-resty-string
-ln -s /opt/lua/modules/nginx/lua-resty-upload
-ln -s /opt/lua/modules/nginx/lua-resty-upstream
-ln -s /opt/lua/modules/nginx/lua-resty-upstream-healthcheck
-ln -s /opt/lua/modules/nginx/lua-resty-lrucache
-ln -s /opt/lua/openresty.org
-ln -s /opt/lua/nginx-tutorials
-ln -s /opt/lua/test-nginx
-cd ~/gitf
-ln -s /opt/lua/modules/forked/lua-resty-shell
-ln -s /opt/lua/modules/forked/lua-pycrypto-aes
-ln -s /opt/lua/modules/forked/sockproc
-ln -s /opt/lua/underscore.lua
-ln -s /opt/lua/nginx_tcp_proxy_module
-ln -s /opt/lua/luajit-examples
-ln -s /opt/lua/docker-openresty
-ln -s /opt/lua/openresty-docker
-cd
-echo "installing rocks..."
-luarocks-5.1 --local install lua-cjson
-luarocks-5.1 --local install lua-iconv
-luarocks-5.1 --local install lua-llthreads2
-luarocks-5.1 --local install luacrypto
-luarocks-5.1 --local install lualogging
-luarocks-5.1 --local install luasec OPENSSL_LIBDIR=/usr/lib64/
-luarocks-5.1 --local install luasql-mysql \
-                         MYSQL_INCDIR=/usr/include/mysql \
-                         MYSQL_LIBDIR=/usr/lib/mysql
-luarocks-5.1 --local install luasql-sqlite3
-luarocks-5.1 --local install redis-lua
-luarocks-5.1 --local install stdlib
-luarocks-5.1 --local install underscore.lua \
-                         --from=http://github.com/irr/underscore.lua/raw/master/rocks
-
+rm -rf sockproc
