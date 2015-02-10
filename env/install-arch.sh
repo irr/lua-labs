7b251895f221c647b9e584e2c9ed90c49b923792
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-02-10 08:09:17 -0200

    antergos

--- env/install-arch.sh
+++ env/install-arch.sh
@@ -8,10 +8,10 @@ cd nginx_tcp_proxy_module
 git remote add upstream https://github.com/yaoweibin/nginx_tcp_proxy_module.git
 git fetch upstream && git merge upstream/master && git push
 cd ..
-wget http://openresty.org/download/ngx_openresty-1.7.7.1.tar.gz
-tar xfva ngx_openresty-1.7.7.1.tar.gz
-cd ngx_openresty-1.7.7.1
-patch -p1 < ../nginx_tcp_proxy_module/tcp-ngx-1.7.7.1.patch
+wget http://openresty.org/download/ngx_openresty-1.7.7.2.tar.gz
+tar xfva ngx_openresty-1.7.7.2.tar.gz
+cd ngx_openresty-1.7.7.2
+patch -p1 < ../nginx_tcp_proxy_module/tcp-ngx-1.7.7.2.patch
 ./configure --prefix=/opt/lua/openresty \
             --with-luajit \
             --with-http_realip_module \
@@ -47,12 +47,76 @@ perl Makefile.PL
 make
 sudo make install
 cd ..
-rm -rf test-nginx
+git clone https://github.com/openresty/openresty.org.git
+git clone https://github.com/openresty/nginx-tutorials.git
+mkdir -p /opt/lua/modules/nginx
+cd /opt/lua/modules/nginx/
+git clone https://github.com/openresty/headers-more-nginx-module.git
+git clone https://github.com/openresty/set-misc-nginx-module.git
+git clone https://github.com/calio/iconv-nginx-module.git
+git clone https://github.com/openresty/lua-nginx-module.git
+git clone https://github.com/openresty/lua-resty-dns.git
+git clone https://github.com/pintsized/lua-resty-http.git
+git clone https://github.com/openresty/lua-resty-memcached.git
+git clone https://github.com/openresty/lua-resty-mysql.git
+git clone https://github.com/openresty/lua-resty-redis.git
+git clone https://github.com/openresty/lua-resty-string.git
+git clone https://github.com/openresty/lua-resty-upload.git
+git clone https://github.com/openresty/lua-resty-upstream-healthcheck.git
+git clone https://github.com/hamishforbes/lua-resty-upstream.git
+git clone https://github.com/openresty/lua-resty-lrucache.git
+git clone https://github.com/bungle/lua-resty-template.git
+mkdir -p /opt/lua/modules/forked
+cd /opt/lua/modules/forked/
+git clone git@github.com:irr/lua-resty-shell.git
+cd lua-resty-shell
+git remote add upstream https://github.com/juce/lua-resty-shell.git
+git fetch upstream && git merge upstream/master && git push
+cd ..
+git clone git@github.com:irr/lua-pycrypto-aes.git
+cd lua-pycrypto-aes
+git remote add upstream https://github.com/siddontang/lua-pycrypto-aes.git
+git fetch upstream && git merge upstream/master && git push
+cd ..
 git clone git@github.com:irr/sockproc.git
 cd sockproc
 git remote add upstream https://github.com/juce/sockproc.git
 git fetch upstream && git merge upstream/master && git push
 make
 sudo mv sockproc /usr/local/bin/
+make clean
 cd ..
-rm -rf sockproc
+git clone http://luajit.org/git/luajit-2.0.git
+cd luajit-2.0
+git checkout v2.1
+cd ..
+git clone git@github.com:irr/luajit-examples.git
+cd luajit-examples
+git remote add upstream https://github.com/hnakamur/luajit-examples.git
+git fetch upstream && git merge upstream/master && git push
+cd ~/git
+ln -s /opt/lua/modules/nginx/headers-more-nginx-module
+ln -s /opt/lua/modules/nginx/set-misc-nginx-module
+ln -s /opt/lua/modules/nginx/iconv-nginx-module
+ln -s /opt/lua/modules/nginx/lua-nginx-module
+ln -s /opt/lua/modules/nginx/lua-resty-dns
+ln -s /opt/lua/modules/nginx/lua-resty-http
+ln -s /opt/lua/modules/nginx/lua-resty-memcached
+ln -s /opt/lua/modules/nginx/lua-resty-mysql
+ln -s /opt/lua/modules/nginx/lua-resty-redis
+ln -s /opt/lua/modules/nginx/lua-resty-string
+ln -s /opt/lua/modules/nginx/lua-resty-upload
+ln -s /opt/lua/modules/nginx/lua-resty-upstream
+ln -s /opt/lua/modules/nginx/lua-resty-upstream-healthcheck
+ln -s /opt/lua/modules/nginx/lua-resty-lrucache
+ln -s /opt/lua/openresty.org
+ln -s /opt/lua/nginx-tutorials
+ln -s /opt/lua/test-nginx
+cd ~/gitf
+ln -s /opt/lua/modules/forked/lua-resty-shell
+ln -s /opt/lua/modules/forked/lua-pycrypto-aes
+ln -s /opt/lua/modules/forked/sockproc
+ln -s /opt/lua/nginx_tcp_proxy_module
+ln -s /opt/lua/luajit-examples
+cd
+
