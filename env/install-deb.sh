da1d50eaeba9e395fe6b41137ac8bb3d18e2d210
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-02-19 07:44:13 -0200

    ubuntu

--- env/install-deb.sh
+++ env/install-deb.sh
@@ -42,6 +42,7 @@ sudo ln -s /opt/lua/openresty/nginx/sbin/nginx
 cd /usr/local/bin
 sudo ln -s /opt/lua/openresty/luajit/bin/luajit-2.1.0-alpha luajit
 sudo ln -s /home/irocha/lua/openresty/luangx/luangx.lua luangx
+sudo ln -s /opt/lua/openresty/bin/resty
 cd /usr/local
 sudo ln -s /opt/lua/openresty openresty
 sudo ln -s /opt/lua/openresty openresty-debug
