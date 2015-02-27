8c43b29c5f8ff3ce4087a7e7daf643584430a368
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-02-27 11:09:55 -0300

    centos6.6

--- configs/init.lua
+++ configs/init.lua
@@ -1,3 +1,6 @@
+package.path = package.path .. ";/home/irocha/.luarocks/share/lua/5.1/?.lua;"
+package.cpath = package.cpath .. ";/home/irocha/.luarocks/lib/lua/5.1/?.so;"
+
 function d(o)
     if type(o) == "table" then
         for k, v in pairs(o) do
