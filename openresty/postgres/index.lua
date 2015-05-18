a13e35d8cb2b03d3c0d693420fcfa1b18cdda55c
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-05-18 16:09:16 -0300

    pgsql

---
+++ openresty/postgres/index.lua
@@ -0,0 +1,56 @@
+local pg = require("resty.postgres")
+
+--------------------------------
+-- LIBRARIES
+--------------------------------
+
+function exit_now(status, msg)
+    if status ~= ngx.HTTP_OK then
+        ngx.status = status
+    end
+    if msg then
+        ngx.say(json.encode(msg))
+    end
+
+    local request_time = (ngx.now() - ngx.req.start_time())
+    ngx.log(ngx.INFO, "request time: " .. tostring(request_time))
+
+    ngx.exit(ngx.HTTP_OK)
+end
+
+function exit(db, rd, status, msg)
+    if status then
+        exit_now(status, msg)
+    end
+    exit_now(ngx.HTTP_OK, msg)
+end
+
+-------------
+-- MAIN
+-------------
+
+ngx.header.content_type = 'application/json';
+
+local db = pg:new()
+db:set_timeout(3000)
+
+local ok, err = db:connect({host="127.0.0.1",port=5432, database=ngx.var.db_database,
+                            user=ngx.var.db_user,password=ngx.var.db_pass,compact=false})
+
+if not ok then
+    ngx.say(err)
+end
+
+local res, err = db:query("SELECT loc_id, loc_name, ST_AsGeoJSON(geog)::json FROM locations;")
+db:set_keepalive(0, 10)
+
+if not err then
+    for i,v in ipairs(res) do
+        ngx.say(string.format("%s\t%s",v.loc_id,v.loc_name, v.st_asgeojson))
+    end
+    exit()
+end
+
+exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+
+
