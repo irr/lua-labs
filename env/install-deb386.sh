2306b59d57686326040efaf0cd3f69a4fcfb547f
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-07-31 10:53:16 -0300

    openresty-1.7.10.2

--- env/install-deb386.sh
+++ env/install-deb386.sh
@@ -34,10 +34,10 @@ sudo cp ~/lua/configs/drizzle7.conf /etc/ld.so.conf.d/
 sudo ldconfig && ldconfig -p |grep drizzle
 cd ..
 rm -rf drizzle7-2011.07.21*
-wget http://openresty.org/download/ngx_openresty-1.7.10.1.tar.gz
-tar xfva ngx_openresty-1.7.10.1.tar.gz
-cd ngx_openresty-1.7.10.1
-patch -p1 < ../nginx_tcp_proxy_module/tcp-ngx-1.7.10.1.patch
+wget http://openresty.org/download/ngx_openresty-1.7.10.2.tar.gz
+tar xfva ngx_openresty-1.7.10.2.tar.gz
+cd ngx_openresty-1.7.10.2
+patch -p1 < ../nginx_tcp_proxy_module/tcp-ngx-1.7.10.2.patch
 ./configure --prefix=/opt/lua/openresty \
             --with-http_gunzip_module \
             --with-luajit \
