78d9d73999cd98866a972e85d5ff398a62d01ddd
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-09-01 15:13:24 -0300

    distance

--- geohash/geohash.lua
+++ geohash/geohash.lua
@@ -27,25 +27,22 @@ BORDERS.top.odd = BORDERS.right.even
 BORDERS.left.odd = BORDERS.bottom.even
 BORDERS.right.odd = BORDERS.top.even
 
+function d(o)
+    if type(o) == "table" then
+        for k, v in pairs(o) do
+            print(k,v)
+        end
+    else
+        print(o)
+    end
+end
+
 function refine_interval(interval, cd, mask)
     if bit.band(cd, mask) > 0 then
         interval[1] = (interval[1] + interval[2]) / 2
     else
         interval[2] = (interval[1] + interval[2]) / 2
     end
-end
-
-function calculate_adjacent(srcHash, dir)
-    srcHash = srcHash:lower()
-    local lenHash = #srcHash
-    local lastChr = srcHash:sub(lenHash, lenHash);
-    local t = ((math.mod(lenHash,2) == 0) and 'even') or 'odd'
-    local base = srcHash:sub(1, lenHash - 1)
-    if BORDERS[dir][t]:find(lastChr) then
-        base = calculate_adjacent(base, dir)
-    end
-    local n = NEIGHBORS[dir][t]:find(lastChr)
-    return base..BASE32:sub(n, n)
 end
 
 function decode(geohash)
@@ -118,23 +115,72 @@ function encode(latitude, longitude, precision)
     return geohash
 end
 
+function calculate_distance(lat1, lon1, lat2, lon2)
+    local R = 6371000
+    local r1 = math.rad(lat1)
+    local r2 = math.rad(lat2)
+    local dlat = math.rad((lat2-lat1))
+    local dlon = math.rad((lon2-lon1))
+    local a = math.sin(dlat/2) * math.sin(dlat/2) +
+              math.cos(r1) * math.cos(r2) *
+              math.sin(dlon/2) * math.sin(dlon/2)
+    local c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    local d = R * c
+    return d
+end
+
+function distance(hash1, hash2)
+    local t1 = decode(hash1)
+    local t2 = decode(hash2)
+    local c1 = coord(t1)
+    local c2 = coord(t2)
+    return calculate_distance(c1.lat, c1.lon, c2.lat, c2.lon)
+end
+
+function neighbor(hash, dir)
+    hash = hash:lower()
+    local len = #hash
+    local last = hash:sub(len, len);
+    local t = ((math.mod(len,2) == 0) and 'even') or 'odd'
+    local base = hash:sub(1, len - 1)
+    if BORDERS[dir][t]:find(last) then
+        base = neighbor(base, dir)
+    end
+    local n = NEIGHBORS[dir][t]:find(last)
+    return base..BASE32:sub(n, n)
+end
+
 function neighbors(geohash)
-    local neighbors = { top = calculate_adjacent(geohash, 'top'),
-                        bottom = calculate_adjacent(geohash, 'bottom'),
-                        right = calculate_adjacent(geohash, 'right'),
-                        left = calculate_adjacent(geohash, 'left') }
-    neighbors['topleft'] = calculate_adjacent(neighbors['left'], 'top');
-    neighbors['topright'] = calculate_adjacent(neighbors['right'], 'top');
-    neighbors['bottomleft'] = calculate_adjacent(neighbors['left'], 'bottom');
-    neighbors['bottomright'] = calculate_adjacent(neighbors['right'], 'bottom');
+    local neighbors = { top = neighbor(geohash, 'top'),
+                        bottom = neighbor(geohash, 'bottom'),
+                        right = neighbor(geohash, 'right'),
+                        left = neighbor(geohash, 'left') }
+    neighbors['topleft'] = neighbor(neighbors['left'], 'top');
+    neighbors['topright'] = neighbor(neighbors['right'], 'top');
+    neighbors['bottomleft'] = neighbor(neighbors['left'], 'bottom');
+    neighbors['bottomright'] = neighbor(neighbors['right'], 'bottom');
     return neighbors
 end
 
+function coord(t)
+    if type(t) == 'table' then
+        return { lat = t.latitude[2], lon = t.longitude[2] }
+    else
+        return coord(decode(t))
+    end
+end
+
+function coord_str(t)
+    local t = coord(t)
+    return string.format("lat: %s and lon: %s", tostring(t.lat), tostring(t.lon))
+end
+
 function test()
-    print("\nencoding", "lat/long (42.6, -5.6)")
-    local h = encode(42.6, -5.6, 12)
-    print("result/decode", h, "\n")
-    local t = decode(h)
+    local precision = 4
+    print("\nencoding", "lat/long (-23.643380, -46.759670)")
+    local h1 = encode(-23.643380, -46.759670, precision)
+    print("result/decode", h1, "\n")
+    local t = decode(h1)
     for _, l in pairs({'latitude', 'longitude'}) do
         print(l)
         for k, v in pairs(t[l]) do
@@ -142,8 +188,20 @@ function test()
         end
     end
     print("\n")
-    for k, v in pairs(neighbors(h)) do
+    local blocks = neighbors(h1)
+    for k, v in pairs(blocks) do
         print(string.format("%10s\t%s", k, v))
     end
     print("\n")
+
+    for p = 4, 12 do
+        local h1 = encode(-23.643380, -46.759670, p)
+        local h2 = encode(-23.569641, -46.691774, p)
+        local dist = distance(h1, h2)
+        print(string.format("using precision = %d", p))
+        print(string.format("distance from %s or %s\n         to   %s or %s\n         = %6.2f meters ~= %2.4f Km\n",
+            h1, coord_str(h1), h2, coord_str(h2), dist, dist/1000))
+    end
 end
+
+test()
