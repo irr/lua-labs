783b5553ef6230a27965335a56d2ef1c587dfe6b
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-10-26 10:19:43 -0200

    fmt

--- openresty/s3/auth.lua
+++ openresty/s3/auth.lua
@@ -1,3 +1,4 @@
+-- http://docs.aws.amazon.com/AmazonS3/latest/API/APIRest.html
 local AWSAccessKeyId = os.getenv("AWS_ACCESS_KEY_ID")
 local AWSSecretAccessKey = os.getenv("AWS_SECRET_ACCESS_KEY")
 local Bucket = "irrs3"
@@ -8,5 +9,5 @@ local Now = ngx.cookie_time(ngx.now())
 local Request = string.format("GET\n\n\n%s\n/%s/%s", Now, Bucket, Key)
 local Digest = ngx.encode_base64(ngx.hmac_sha1(AWSSecretAccessKey, Request))
 
-ngx.say(string.format("\ncurl -v -H \"Date: %s\" -H \"Authorization: AWS %s:%s\" http://%s/%s\n", Now, AWSAccessKeyId, Digest, Host, Key))
+ngx.say(string.format("\ncurl -v -H \"Date: %s\" -H \"Authorization: AWS %s:%s\" http://%s/%s", Now, AWSAccessKeyId, Digest, Host, Key) .. ";echo")
 
