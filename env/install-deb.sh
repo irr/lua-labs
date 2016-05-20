f2f1e9e86c74bed285f252c14e5f9a44e6537d62
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-05-20 15:19:04 -0300

    openresty/1.9.15.1rc1

--- env/install-deb.sh
+++ env/install-deb.sh
@@ -48,7 +48,7 @@ echo "creating symlinks..."
 cd /usr/sbin
 sudo ln -s /opt/lua/openresty/nginx/sbin/nginx
 cd /usr/local/bin
-sudo ln -s /opt/lua/openresty/luajit/bin/luajit-2.1.0-beta1 luajit
+sudo ln -s /opt/lua/openresty/luajit/bin/luajit-2.1.0-beta2 luajit
 sudo ln -s /opt/lua/openresty/bin/resty
 cd /usr/local
 sudo ln -s /opt/lua/openresty openresty
