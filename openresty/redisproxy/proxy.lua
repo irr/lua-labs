6bb779da0d6dd6f489358db39f4d6a6d22a12f1a
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-07-06 09:49:11 -0300

    upstreams

--- openresty/redisproxy/proxy.lua
+++ openresty/redisproxy/proxy.lua
@@ -1,31 +1,42 @@
 --[[
 redis-server
-redis-cli set test www.uol.com.br
-http http://localhost:8080?id=test
+redis-cli set uol uol.com.br && echo -e "upstream uol.com.br {\n\tserver uol.com.br;\n}" > upstreams/uol.conf && nginx -s reload
+redis-cli set irr irrlab.com && echo -e "upstream irrlab.com {\n\tserver irrlab.com;\n}" > upstreams/irr.conf && nginx -s reload
+http http://localhost:8080?id=uol
+http http://localhost:8080?id=irr
 --]]
 
 local keys = ngx.req.get_uri_args()
 local key = keys["id"]
-local res = ngx.location.capture(
-    "/redis", { args = { key = key } }
-)
 
-if res.status ~= 200 then
-    ngx.log(ngx.ERR, "redis server returned bad status: ",
-        res.status)
-    ngx.exit(res.status)
+local route, err = ngx.shared.routes:get(key)
+
+if route then
+    ngx.var.target = route
+else
+    local res = ngx.location.capture(
+        "/redis", { args = { key = key } }
+    )
+
+    if res.status ~= 200 then
+        ngx.log(ngx.ERR, "redis server returned bad status: ", res.status)
+        ngx.exit(res.status)
+    end
+
+    if not res.body then
+        ngx.log(ngx.ERR, "redis returned empty body")
+        ngx.exit(500)
+    end
+
+    local parser = require "redis.parser"
+    local server, typ = parser.parse_reply(res.body)
+    if typ ~= parser.BULK_REPLY or not server then
+        ngx.log(ngx.ERR, "bad redis response: ", res.body)
+        ngx.exit(501)
+    else
+        ngx.shared.routes:set(key, server, ngx.var.throttle)
+        ngx.var.target = server
+    end
 end
 
-if not res.body then
-    ngx.log(ngx.ERR, "redis returned empty body")
-    ngx.exit(500)
-end
-
-local parser = require "redis.parser"
-local server, typ = parser.parse_reply(res.body)
-if typ ~= parser.BULK_REPLY or not server then
-    ngx.log(ngx.ERR, "bad redis response: ", res.body)
-    ngx.exit(500)
-end
-
-ngx.var.target = server
+ngx.log(ngx.ERR, "target=" .. ngx.var.target)
