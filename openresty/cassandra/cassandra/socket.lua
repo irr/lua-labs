95841a8fa6f1656deca2950f34f82638c52e8a84
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-07-06 15:46:06 -0300

    cassandra

--- openresty/cassandra/cassandra/socket.lua
+++ openresty/cassandra/cassandra/socket.lua
@@ -1,111 +1,135 @@
-local get_phase, ngx_socket, has_cosocket, log, warn
+local tostring = tostring
+local pairs = pairs
+local type = type
 
---- ngx_lua utils
+----------------------------
+-- LuaSocket proxy metatable
+----------------------------
 
-if ngx ~= nil then
-  log = ngx.log
-  warn = ngx.WARN
-  get_phase = ngx.get_phase
-  ngx_socket = ngx.socket
-  has_cosocket = function()
-    local phase = get_phase()
-    return phase == "rewrite" or phase == "access"
-        or phase == "content" or phase == "timer", phase
+local proxy_mt = {
+  getreusedtimes = function() return 0 end,
+  settimeout = function(self, t)
+    if t then
+      t = t/1000
+    end
+    self.sock:settimeout(t)
+  end,
+  setkeepalive = function(self)
+    self.sock:close()
+    return true
+  end,
+  sslhandshake = function(self, reused_session, _, verify, opts)
+    opts = opts or {}
+    local return_bool = reused_session == false
+
+    local ssl = require 'ssl'
+    local params = {
+      mode = 'client',
+      protocol = 'tlsv1',
+      key = opts.key,
+      certificate = opts.cert,
+      cafile = opts.cafile,
+      verify = verify and 'peer' or 'none',
+      options = 'all'
+    }
+
+    local sock, err = ssl.wrap(self.sock, params)
+    if not sock then
+      return return_bool and false or nil, err
+    end
+
+    local ok, err = sock:dohandshake()
+    if not ok then
+      return return_bool and false or nil, err
+    end
+
+    -- purge memoized closures
+    for k, v in pairs(self) do
+      if type(v) == 'function' then
+        self[k] = nil
+      end
+    end
+
+    self.sock = sock
+
+    return return_bool and true or self
   end
-else
-  log = function()end
-  get_phase = function()end
-  has_cosocket = function()end
+}
+
+proxy_mt.__tostring = function(self)
+  return tostring(self.sock)
 end
 
---- LuaSocket proxy metatable
-
-local luasocket_mt = {}
-
-function luasocket_mt:__index(key)
-  local override = rawget(luasocket_mt, key)
-  if override ~= nil then
+proxy_mt.__index = function(self, key)
+  local override = proxy_mt[key]
+  if override then
     return override
   end
 
   local orig = self.sock[key]
-  if type(orig) == "function" then
+  if type(orig) == 'function' then
     local f = function(_, ...)
       return orig(self.sock, ...)
     end
     self[key] = f
     return f
+  elseif orig then
+    return orig
   end
-
-  return orig
 end
 
+-----------------------
+-- ngx_lua/plain compat
+-----------------------
 
---- LuaSocket <-> ngx_lua compat
+local new_tcp
 
-function luasocket_mt.getreusedtimes()
-  return 0
-end
+do
+  local setmetatable = setmetatable
 
-function luasocket_mt:settimeout(t)
-  self.sock:settimeout(t/1000)
-end
+  if ngx then
+    local log, WARN = ngx.log, ngx.WARN
+    local get_phase = ngx.get_phase
+    local ngx_socket = ngx.socket
 
-function luasocket_mt:setkeepalive()
-  self.sock:close()
-  return true
-end
+    local cosocket_phases = {
+      rewrite = true,
+      access = true,
+      content = true,
+      timer = true
+    }
 
---- Perform SSL handshake.
--- Mimics the ngx_lua `sslhandshake()` signature with an additional argument
--- to specify other SSL options for plain Lua.
-function luasocket_mt:sslhandshake(reused_session, _, verify, luasec_opts)
-  luasec_opts = luasec_opts or {}
-  local return_bool = reused_session == false
+    new_tcp = function(...)
+      local phase = get_phase()
+      if cosocket_phases[phase] then
+        return ngx_socket.tcp(...)
+      elseif phase ~= 'init' then
+        log(WARN, 'no support for cosockets in this context, falling back on LuaSocket')
+      end
 
-  local ssl = require "ssl"
-  local params = {
-    mode = "client",
-    protocol = "tlsv1",
-    key = luasec_opts.key,
-    certificate = luasec_opts.certificate,
-    cafile = luasec_opts.ca,
-    verify = verify and "peer" or "none",
-    options = "all"
-  }
+      local socket = require 'socket'
 
-  local ssl_sock, err = ssl.wrap(self.sock, params)
-  if err then
-    return return_bool and false or nil, err
+      return setmetatable({
+        sock = socket.tcp(...)
+      }, proxy_mt)
+    end
+  else
+    local socket = require 'socket'
+
+    new_tcp = function(...)
+      return setmetatable({
+        sock = socket.tcp(...)
+      }, proxy_mt)
+    end
   end
-
-  local ok, err = ssl_sock:dohandshake()
-  if not ok then
-    return return_bool and false or nil, err
-  end
-
-  self.sock = ssl_sock
-
-  return return_bool and true or ssl_sock
 end
 
---- Module
+---------
+-- Module
+---------
 
 return {
-  tcp = function(...)
-    local ok, phase = has_cosocket()
-    if ok then
-      return ngx_socket.tcp(...)
-    elseif phase ~= "init" then
-      log(warn, "no support for cosockets in this context, falling back on LuaSocket")
-    end
-
-    local socket = require "socket"
-
-    return setmetatable({
-      sock = socket.tcp(...)
-    }, luasocket_mt)
-  end,
-  luasocket_mt = luasocket_mt,
-  _VERSION = "0.0.2"
+  tcp = new_tcp,
+  luasocket_mt = proxy_mt,
+  _VERSION = '0.0.5'
 }
