c80fb71bb704ae366eab0f4c8c692a630dcf9f88
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-07-07 10:46:52 -0300

    post-check

--- openresty/redisproxy/proxy.lua
+++ openresty/redisproxy/proxy.lua
@@ -10,35 +10,26 @@ curl -v http://localhost:8080?id=irr
 curl -s -H "Content-Type: application/json" -X POST -d '{"id":"uol"}' http://localhost:8080|head -10
 --]]
 
-local keys = nil
+local ok, keys = nil, nil
 
 if ngx.req.get_method() == "GET" or ngx.req.get_method() == "HEAD" then
     keys = ngx.req.get_uri_args()
 elseif ngx.req.get_method() == "POST" then
     ngx.req.read_body()
     local json = require "cjson"
-    local ok, data = pcall(json.decode, ngx.req.get_body_data())
-    if ok then
-        keys = data
+    ok, keys = pcall(json.decode, ngx.req.get_body_data())
+    if not ok then
+        ngx.log(ngx.ERR, "id could not be extracted")
+        ngx.exit(400)
     end
 end
 
-if not keys then
-    ngx.log(ngx.ERR, "id could not be extracted")
-    ngx.exit(400)
-end
-
 local key = keys["id"]
-
 local route, err = ngx.shared.routes:get(key)
-
 if route then
     ngx.var.target = route
 else
-    local res = ngx.location.capture(
-        "/redis", { args = { key = key } }
-    )
-
+    local res = ngx.location.capture("/redis", { args = { key = key } })
     if res.status ~= 200 then
         ngx.log(ngx.ERR, "redis bad status: ", res.status)
         ngx.exit(res.status)
