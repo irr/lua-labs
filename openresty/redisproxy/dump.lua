b16b4435d1b369e462d4b3f22cddcbd60d0a8b5a
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-07-07 09:24:23 -0300

    dump

---
+++ openresty/redisproxy/dump.lua
@@ -0,0 +1,17 @@
+--[[
+curl -v -H "Content-Type: application/json" -X POST -d '{"id":"none", "data": {"name": "luma"}}' http://localhost:8080
+]]
+
+local json = require "cjson"
+
+local data = nil
+
+if ngx.req.get_method() == "GET" or ngx.req.get_method() == "HEAD" then
+    data = ngx.req.get_uri_args()
+elseif ngx.req.get_method() == "POST" then
+    ngx.req.read_body()
+    data = ngx.req.get_body_data()
+end
+
+ngx.header.content_type = 'application/json';
+ngx.say(json.encode(data))
